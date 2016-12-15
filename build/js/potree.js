55888dbf998acd2fc2f5e588130ac45539a8037a
AdamWilliamson <AdamWilliamson@hexagonmining.com>
2016-12-15 12:36:15 +0800

    Update potree to allow reloading EDL shader

--- build/js/potree.js
+++ build/js/potree.js
@@ -5831,28 +5831,29 @@ Potree.utils.frustumSphereIntersection = function(frustum, sphere){
 };
 	
 	
-Potree.utils.screenPass = new function(){
+Potree.utils.screenPassConstructor =function(){
 
-	this.screenScene = new THREE.Scene();
-	this.screenQuad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2, 0));
-	this.screenQuad.material.depthTest = true;
-	this.screenQuad.material.depthWrite = true;
-	this.screenQuad.material.transparent = true;
-	this.screenScene.add(this.screenQuad);
-	this.camera = new THREE.Camera();
-	
-	this.render = function(renderer, material, target){
-		this.screenQuad.material = material;
-		
-		if(typeof target === undefined){
-			renderer.render(this.screenScene, this.camera);
-		}else{
-			renderer.render(this.screenScene, this.camera, target);
-		}
-	}
-}();
-	
-	
+    this.screenScene = new THREE.Scene();
+    this.screenQuad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2, 0));
+    this.screenQuad.material.depthTest = true;
+    this.screenQuad.material.depthWrite = true;
+    this.screenQuad.material.transparent = true;
+    this.screenScene.add(this.screenQuad);
+    this.camera = new THREE.Camera();
+
+    this.render = function(renderer, material, target){
+        this.screenQuad.material = material;
+
+        if(typeof target === undefined){
+            renderer.render(this.screenScene, this.camera);
+        }else{
+            renderer.render(this.screenScene, this.camera, target);
+        }
+    }
+}
+
+Potree.utils.screenPass = new Potree.utils.screenPassConstructor();
+
 	
 
 Potree.Features = function(){
--- src/utils.js
+++ src/utils.js
@@ -304,28 +304,29 @@ Potree.utils.frustumSphereIntersection = function(frustum, sphere){
 
 	return (minDistance >= sphere.radius) ? 2 : 1;
 };
-	
-	
-Potree.utils.screenPass = new function(){
 
-	this.screenScene = new THREE.Scene();
-	this.screenQuad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2, 0));
-	this.screenQuad.material.depthTest = true;
-	this.screenQuad.material.depthWrite = true;
-	this.screenQuad.material.transparent = true;
-	this.screenScene.add(this.screenQuad);
-	this.camera = new THREE.Camera();
+Potree.utils.screenPassConstructor =function(){
+
+    this.screenScene = new THREE.Scene();
+    this.screenQuad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2, 0));
+    this.screenQuad.material.depthTest = true;
+    this.screenQuad.material.depthWrite = true;
+    this.screenQuad.material.transparent = true;
+    this.screenScene.add(this.screenQuad);
+    this.camera = new THREE.Camera();
+
+    this.render = function(renderer, material, target){
+        this.screenQuad.material = material;
+
+        if(typeof target === undefined){
+            renderer.render(this.screenScene, this.camera);
+        }else{
+            renderer.render(this.screenScene, this.camera, target);
+        }
+    }
+}
 	
-	this.render = function(renderer, material, target){
-		this.screenQuad.material = material;
-		
-		if(typeof target === undefined){
-			renderer.render(this.screenScene, this.camera);
-		}else{
-			renderer.render(this.screenScene, this.camera, target);
-		}
-	}
-}();
+Potree.utils.screenPass = new Potree.utils.screenPassConstructor();
 	
 	
 	
